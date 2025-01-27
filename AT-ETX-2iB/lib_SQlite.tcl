3262515200b34de698f3ae3d71d4577a65709615
Ilya Ginzburg <ilya_g@rad.com>
2025-01-27 15:38:37 +0200

    Use ::RLWS::UpdateDB2 to update TCC (SQliteAddLine)

--- AT-ETX-2iB/lib_SQlite.tcl
+++ AT-ETX-2iB/lib_SQlite.tcl
@@ -73,7 +73,8 @@ proc SQliteAddLine {pair} {
 
 
   for {set tr 1} {$tr <= 6} {incr tr} {
-    if [catch {UpdateDB $barcode $uut $hostDescription $date $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator} res] {
+    #if [catch {UpdateDB $barcode $uut $hostDescription $date $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator} res] {}
+    if [catch {::RLWS::UpdateDB2 $barcode $uut $hostDescription $date $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator "" "" "" "" ""} res] {
       set res "Try${tr}_fail.$res"
       puts "[MyTime] Web DataBase is not updated. Try:<$tr>. Res:<$res>" ; update
       after [expr {int(rand()*3000+60)}] 
